9ffd55edbb7acbb78112ea2c0be771387683f9ea
HM The Mister Techno <giorgos4006@gmail.com>
2021-11-03 16:43:46 +0200

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -9,7 +9,7 @@
 function CompileVisualL10N() {
 
 /* Visual Themes */
-var visualColorNames_l10n = [mw.msg( 'tunic-color-factorycolors' ), mw.msg( 'tunic-color-lunacolors' ), mw.msg( 'tunic-color-classicforced' ), mw.msg( 'tunic-color-campbellforced' ), mw.msg( 'tunic-color-forced' ), mw.msg( 'tunic-color-tangoforced' ),mw.msg( 'tunic-color-rgbcolors' ),mw.msg( 'tunic-color-retro' ),mw.msg( 'tunic-color-retro' ) + ' II',mw.msg( 'tunic-color-retro' ) + ' III',mw.msg( 'tunic-color-retro' ) + ' IV',mw.msg( 'tunic-color-retro' ) + ' V',mw.msg( 'tunic-color-retro' ) + ' VI',mw.msg( 'tunic-color-retro' ) + ' VII',mw.msg( 'tunic-color-fandomcolors' ) ];
+var visualColorNames_l10n = [mw.msg( 'tunic-color-factorycolors' ), mw.msg( 'tunic-color-lunacolors' ), mw.msg( 'tunic-color-classicforced' ), mw.msg( 'tunic-color-classicforced' ) + ' II', mw.msg( 'tunic-color-campbellforced' ), mw.msg( 'tunic-color-forced' ), mw.msg( 'tunic-color-forced' ) + ' II', mw.msg( 'tunic-color-forced' ) + ' III', mw.msg( 'tunic-color-tangoforced' ),mw.msg( 'tunic-color-rgbcolors' ),mw.msg( 'tunic-color-retro' ),mw.msg( 'tunic-color-retro' ) + ' II',mw.msg( 'tunic-color-retro' ) + ' III',mw.msg( 'tunic-color-retro' ) + ' IV',mw.msg( 'tunic-color-retro' ) + ' V',mw.msg( 'tunic-color-retro' ) + ' VI',mw.msg( 'tunic-color-retro' ) + ' VII',mw.msg( 'tunic-color-fandomcolors' ),mw.msg( 'tunic-color-fandomcolors' ) + ' II',mw.msg( 'tunic-color-fandomcolors' ) + ' III', mw.msg( 'tunic-color-candycrush' ), mw.msg( 'tunic-color-candycrush' ) + ' II' , mw.msg( 'tunic-color-discord' ),'tunic OS'];
 
 // Changes Strings into l10ned version
 /* Visual Colors */
